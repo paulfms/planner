9bdb77b6e041c7452f46d2810c6cb3c70e56a21f
Paul Anderson <pauldevop@gmail.com>
2024-07-14 15:35:49 -0300

    Creating invite list

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 
 export function TripDetailsPage() {
     return (
@@ -92,14 +92,40 @@ export function TripDetailsPage() {
                                 </div>
                                 <Link2 className="text-zinc-400 size-5 shrink-0" />
                             </div>
-
-                            <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-                                <Plus className="size-5" />
-                                Cadastrar novo link
-                            </button>
-
-                            <div className='w-full h-px bg-zinc-800' />
                         </div>
+                        <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                            <Plus className="size-5" />
+                            Cadastrar novo link
+                        </button>
+                    </div>
+                    <div className='w-full h-px bg-zinc-800' />
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Convidados</h2>
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Jessica White</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                    jessica.white44@yahoo.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                    lacy.stiedemann@gmail.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+                        </div>
+                        <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                            <UserCog className="size-5" />
+                            Gerenciar convidados
+                        </button>
                     </div>
                 </div>
             </main>
