29cde38ce6d1dcdedc38ab4ba71ff495363458f8
Paul Anderson <pauldevop@gmail.com>
2024-07-14 14:43:43 -0300

    Creating Activities List Details

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,3 +1,77 @@
-export function TripDetailsPage () {
-    return <h1>Trip Details!</h1>
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+
+export function TripDetailsPage() {
+    return (
+        <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+            <div className="px-4 h-16 rounded-xl bg-zinc-900 flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <MapPin className="size-5 text-zinc-400" />
+                    <span className="text-zinc-100">São Paulo, Brasil</span>
+                </div>
+
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-2">
+                        <Calendar className="size-5 text-zinc-400" />
+                        <span className="text-zinc-100">17 a 23 de Agosto</span>
+                    </div>
+
+                    <div className='w-px h-6 bg-zinc-800' />
+
+                    <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                        Alterar local/data
+                        <Settings2 />
+                    </button>
+                </div>
+            </div>
+
+            <main className="flex gap-16 px-4">
+                <div className="flex-1 space-y-6">
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-3xl font-semibold">Atividades</h2>
+                        <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                            <Plus className='size-5' />
+                            Cadastrar atividade
+                        </button>
+
+                    </div>
+
+
+                    <div className="space-y-8">
+                        <div className="space-y-2.5">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+                                <span className="text-xs text-zinc-500">Sábado</span>
+                            </div>
+                            <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+                        </div>
+
+
+                        <div className="space-y-2.5">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
+                                <span className="text-xs text-zinc-500">Domingo</span>
+                            </div>
+                            <div className="space-y-2">
+
+                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <span className="text-zinc-100">Academia em grupo</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                                </div>
+                                
+                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <span className="text-zinc-100">Academia em grupo</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-80"></div>
+
+            </main>
+        </div>
+    )
 }
